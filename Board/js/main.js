c2700a2c811e7c31c977a28f6e844f1ac9837d4a
Yuriy Roy <roy.yuri@gmail.com>
2015-09-29 00:07:45 +0300

    Layout works

--- Board/js/main.js
+++ Board/js/main.js
@@ -2,7 +2,18 @@
  * Created by Yuri on 9/12/2015.
  */
 
-var app = angular.module('messageBoard', []);
+var messageBoard = angular.module('messageBoard', []);
 
-
-
+messageBoard.controller('MessageBoardCtrl', function ($scope) {
+    $scope.messages = [
+        {'title': 'Lets make some flood',
+            'text': 'Can we speak about something',
+            'reply': 'Yes, I suggest girls'},
+        {'title': 'Go for a drag',
+            'text': 'This sunday at usual time',
+            'reply': 'Aha, on second Marshall'},
+        {'title': 'Suggest a moto-service ',
+            'text': 'I need to change oil, clean a chain, and check overall technical condition',
+            'reply': 'You can try Roma at Stalingrad Heroes Ave.'}
+    ];
+});
