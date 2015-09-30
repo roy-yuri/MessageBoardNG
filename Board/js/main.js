9dcaffbccc4733338337d558d08bf91c01c7ab0f
Yuriy Roy <roy.yuri@gmail.com>
2015-09-30 23:24:10 +0300

    More layout works, change model with typical forum db representation. I.e. plain list of posts with links on parents.

--- Board/js/main.js
+++ Board/js/main.js
@@ -6,14 +6,29 @@ var messageBoard = angular.module('messageBoard', []);
 
 messageBoard.controller('MessageBoardCtrl', function ($scope) {
     $scope.messages = [
-        {'title': 'Lets make some flood',
-            'text': 'Can we speak about something',
-            'reply': 'Yes, I suggest girls'},
-        {'title': 'Go for a drag',
-            'text': 'This sunday at usual time',
-            'reply': 'Aha, on second Marshall'},
-        {'title': 'Suggest a moto-service ',
-            'text': 'I need to change oil, clean a chain, and check overall technical condition',
-            'reply': 'You can try Roma at Stalingrad Heroes Ave.'}
+        {'id': 1,
+            'parent':'root',
+            'title': 'Lets make some flood',
+            'text': 'Can we speak about something'},
+        {'id': 2,
+            'parent':1,
+            'title': 'Lets make some flood',
+            'text': 'Yes, I suggest girls'},
+        {'id': 3,
+            'parent':1,
+            'title': 'Lets make some flood',
+            'text': 'Drag is on this Sun usual time'},
+        {'id': 4,
+            'parent':1,
+            'title': 'Lets make some flood',
+            'text': 'Aha, on secong Marshall'},
+        {'id': 5,
+            'parent':'root',
+            'title': 'Suggest a moto-service',
+            'text': 'I need to change oil, clean a chain, and check overall technical condition'},
+        {'id': 6,
+            'parent':5,
+            'title': 'Suggest a moto-service',
+            'text': 'You can try Roma at Stalingrad Heroes Ave'}
     ];
 });
